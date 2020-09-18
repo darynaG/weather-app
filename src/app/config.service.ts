92acc51c4f4d48b73542a3c902d518dfae8b1aac
Daryna Hatsosh <daryna.hatsosh@edvantis.com>
2020-09-18 18:15:40 +0300

    fixed api link

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class ConfigService {
 
   public static getWeatherApiUrl() {
-    return 'http://api.openweathermap.org/data/2.5/onecall';
+    return 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/onecall';
   }
 
   public static getApiId() {
