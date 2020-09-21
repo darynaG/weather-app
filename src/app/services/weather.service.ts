2ca07e90f8c3c0e5dad74b7c650603fc4512673c
Daryna Hatsosh <daryna.hatsosh@edvantis.com>
2020-09-21 11:57:21 +0300

    fixed import error

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {ConfigService} from './config.service';
-import {LocationCoordinates, Units} from '../helpers/LocationCoordinates';
+import {LocationCoordinates} from '../helpers/LocationCoordinates';
+import {Units} from '../helpers/Units';
 
 @Injectable({
   providedIn: 'root'
